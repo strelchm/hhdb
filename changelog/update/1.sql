9c62993278fba6d0dbaa8c3e38e352f1471e265b
strelchm <strelchm@yandex.ru>
2022-01-30 18:03:53 +0700

    Code-style refactoring

--- changelog/update/1.sql
+++ changelog/update/1.sql
@@ -1,70 +1,70 @@
 -- Specialization (group 4 professions)
 CREATE TABLE specialisation
 (
-    id    bigserial PRIMARY KEY,
-    title varchar
+    id    BIGSERIAL PRIMARY KEY,
+    title VARCHAR
 );
 
 -- Profession
 CREATE TABLE spec_profession
 (
-    id                bigserial PRIMARY KEY,
-    title              varchar,
-    specialisation_id bigserial,
+    id                BIGSERIAL PRIMARY KEY,
+    title             VARCHAR,
+    specialisation_id BIGSERIAL,
     FOREIGN KEY (specialisation_id) REFERENCES specialisation (id)
 );
 
 -- Skill that is connected with profession (todo {strelchm}: skill to profession as many to many?)
 CREATE TABLE skill
 (
-    id                 uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    title               varchar,
-    spec_profession_id bigserial,
+    id                 UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    title              VARCHAR,
+    spec_profession_id BIGSERIAL,
     FOREIGN KEY (spec_profession_id) REFERENCES spec_profession (id)
 );
 
 -- Employment type (connected to day scheduling)
 CREATE TABLE employment_type
 (
-    id    bigserial PRIMARY KEY,
-    title varchar
+    id    BIGSERIAL PRIMARY KEY,
+    title VARCHAR
 );
 
 -- Grouped work experience
 CREATE TABLE work_experience
 (
-    id    bigserial PRIMARY KEY,
-    title varchar
+    id    BIGSERIAL PRIMARY KEY,
+    title VARCHAR
 );
 
 -- todo {strelchm} - to expand regions to towns, cities etc
 CREATE TABLE region
 (
-    id   bigserial PRIMARY KEY,
-    name varchar
+    id   BIGSERIAL PRIMARY KEY,
+    name VARCHAR
 );
 
 CREATE TABLE organisation
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name        varchar,
-    description text
+    name        VARCHAR,
+    description TEXT
 );
 
 CREATE TABLE hh_user
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    password    varchar,
-    create_date timestamp
+    password    VARCHAR,
+    create_date TIMESTAMP
 );
 
 -- hr from organization
 CREATE TABLE employer
 (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name       varchar,
-    company_id uuid,
-    user_id    uuid,
+    name       VARCHAR,
+    company_id UUID,
+    user_id    UUID,
     FOREIGN KEY (user_id) REFERENCES hh_user (id),
     FOREIGN KEY (company_id) REFERENCES organisation (id)
 );
@@ -72,25 +72,25 @@ CREATE TABLE employer
 CREATE TABLE employee
 (
     id      UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id uuid,
-    name    varchar,
+    user_id UUID,
+    name    VARCHAR,
     FOREIGN KEY (user_id) REFERENCES hh_user (id)
 );
 
 CREATE TABLE resume
 (
     id                 UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    title              varchar,
-    compensation_from  double precision,
-    compensation_to    double precision,
-    description        text,
-    area_id            bigserial,
-    work_experience_id bigserial,
-    employment_type_id bigserial,
-    specialisation_id  bigserial,
-    employee_id        uuid,
-    create_date        timestamp,
-    edit_date          timestamp,
+    title              VARCHAR,
+    compensation_from  DOUBLE PRECISION,
+    compensation_to    DOUBLE PRECISION,
+    description        TEXT,
+    area_id            BIGSERIAL,
+    work_experience_id BIGSERIAL,
+    employment_type_id BIGSERIAL,
+    specialisation_id  BIGSERIAL,
+    employee_id        UUID,
+    create_date        TIMESTAMP,
+    edit_date          TIMESTAMP,
     FOREIGN KEY (area_id) REFERENCES region (id),
     FOREIGN KEY (work_experience_id) REFERENCES work_experience (id),
     FOREIGN KEY (employment_type_id) REFERENCES employment_type (id),
@@ -101,8 +101,8 @@ CREATE TABLE resume
 CREATE TABLE resume_skills
 (
     id        UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    resume_id uuid,
-    skill_id  uuid,
+    resume_id UUID,
+    skill_id  UUID,
     FOREIGN KEY (resume_id) REFERENCES resume (id),
     FOREIGN KEY (skill_id) REFERENCES skill (id)
 );
@@ -110,17 +110,17 @@ CREATE TABLE resume_skills
 CREATE TABLE vacancy
 (
     id                 UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    title              varchar,
-    description        text,
-    compensation_from  double precision,
-    compensation_to    double precision,
-    area_id            bigserial,
-    work_experience_id bigserial,
-    specialisation_id  bigserial,
-    employment_type_id bigserial,
-    hr_id        uuid,
-    company_id         uuid,
-    create_date        timestamp,
+    title              VARCHAR,
+    description        TEXT,
+    compensation_from  DOUBLE PRECISION,
+    compensation_to    DOUBLE PRECISION,
+    area_id            BIGSERIAL,
+    work_experience_id BIGSERIAL,
+    specialisation_id  BIGSERIAL,
+    employment_type_id BIGSERIAL,
+    hr_id              UUID,
+    company_id         UUID,
+    create_date        TIMESTAMP,
     FOREIGN KEY (area_id) REFERENCES region (id),
     FOREIGN KEY (work_experience_id) REFERENCES work_experience (id),
     FOREIGN KEY (specialisation_id) REFERENCES specialisation (id),
@@ -132,8 +132,8 @@ CREATE TABLE vacancy
 CREATE TABLE vacancy_skills
 (
     id         UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    vacancy_id uuid,
-    skill_id   uuid,
+    vacancy_id UUID,
+    skill_id   UUID,
     FOREIGN KEY (vacancy_id) REFERENCES vacancy (id),
     FOREIGN KEY (skill_id) REFERENCES skill (id)
 );
@@ -142,12 +142,12 @@ CREATE TABLE vacancy_skills
 CREATE TABLE response
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    resume_id   uuid,
-    vacancy_id  uuid,
-    company_id  uuid,
-    employee_id uuid,
-    employer_id uuid,
-    create_date timestamp,
+    resume_id   UUID,
+    vacancy_id  UUID,
+    company_id  UUID,
+    employee_id UUID,
+    employer_id UUID,
+    create_date TIMESTAMP,
     FOREIGN KEY (resume_id) REFERENCES resume (id),
     FOREIGN KEY (vacancy_id) REFERENCES vacancy (id),
     FOREIGN KEY (company_id) REFERENCES organisation (id),
@@ -158,19 +158,19 @@ CREATE TABLE response
 CREATE TABLE chat
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    title       varchar,
-    response_id uuid,
-    create_date timestamp,
+    title       VARCHAR,
+    response_id UUID,
+    create_date TIMESTAMP,
     FOREIGN KEY (response_id) REFERENCES response (id)
 );
 
 CREATE TABLE message
 (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    text        text,
-    employee_id uuid,
-    employer_id uuid,
-    create_date timestamp,
+    text        TEXT,
+    employee_id UUID,
+    employer_id UUID,
+    create_date TIMESTAMP,
     FOREIGN KEY (employee_id) REFERENCES employee (id),
     FOREIGN KEY (employer_id) REFERENCES employer (id)
 );
